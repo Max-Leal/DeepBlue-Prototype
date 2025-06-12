66c8e39d123564a694f98f3a2607a8fc90ffc857
MatheusMiguelSamp <matheus.m.samp@outlook.com>
2025-06-12 13:55:26 -0300

    Colocado os headers e footers de maneira otimizada usando js e html, e adicionado o mapaInterativo.html

---
+++ src/main/webapp/static/js/footer.js
@@ -0,0 +1,6 @@
+fetch('../static/html/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  })
+  .catch(error => console.error('Erro ao carregar o header:', error));
