9c5a4c49b1245600b235727531109d0830cde14f
Max-leal <max13leal@gmail.com>
2025-07-15 14:06:57 -0300

    Melhorando o backend de usuário

--- src/main/java/daos/UsuarioDao.java
+++ src/main/java/daos/UsuarioDao.java
@@ -9,112 +9,111 @@ import utils.ConexaoDB;
 
 public class UsuarioDao {
 
-	public static Usuario getUsuarioByEmail(String email) {
-		Usuario usuario = new Usuario();
-		try {
-			Connection con = ConexaoDB.getConexao();
-			String sql = "select * from tb_usuario where email = ?";
-			PreparedStatement stm = con.prepareStatement(sql);
-			stm.setString(1, email);
-			ResultSet rs = stm.executeQuery();
-			
-			if (rs.next()) {
-				usuario = new Usuario();
-				usuario.setId(rs.getLong("id"));
-				usuario.setNome(rs.getString("nome"));
-				usuario.setData_nascimento(rs.getDate("data_nascimento").toLocalDate());
-				usuario.setCpf(rs.getString("cpf"));
-				usuario.setEmail(rs.getString("email"));
-				usuario.setSenha(rs.getString("senha"));
-			}
-			rs.close();
-			stm.close();
-			con.close();
-			return usuario;	
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
-	public static void insert(Usuario usuario) {
-		try {
-			Connection con = ConexaoDB.getConexao();
-			String sql = "insert into tb_usuario (nome, data_nascimento, cpf, email, senha) values (?, ?, ?, ?, ?)";
-			PreparedStatement stm = con.prepareStatement(sql);
-			stm.setString(1, usuario.getNome());
-			stm.setDate(2, java.sql.Date.valueOf(usuario.getData_nascimento()));
-			stm.setString(3, usuario.getCpf());
-			stm.setString(4, usuario.getEmail());
-			stm.setString(5, usuario.getSenha());
-			stm.execute();
+    public static Usuario getUsuarioByEmail(String email) {
+        Usuario usuario = null;
+        try {
+            Connection con = ConexaoDB.getConexao();
+            String sql = "SELECT * FROM tb_usuario WHERE email = ?";
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, email);
+            ResultSet rs = stm.executeQuery();
 
-			stm.close();
-			con.close();
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
+            if (rs.next()) {
+                usuario = new Usuario();
+                usuario.setId(rs.getLong("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));
+                usuario.setFoto(rs.getString("foto"));
+            }
 
-	public static Usuario getUsuarioById(int id) {
-		try {
-			Connection con = ConexaoDB.getConexao();
-			String sql = "select * from tb_usuario where id=?";
-			PreparedStatement stm = con.prepareStatement(sql);
-			stm.setInt(1, id);
-			ResultSet rs = stm.executeQuery();
-			Usuario usuario = null;
-			if (rs.next()) {
-				usuario = new Usuario();
-				usuario.setId(rs.getLong("id"));
-				usuario.setNome(rs.getString("nome"));
-				usuario.setData_nascimento(rs.getDate("data_nascimento").toLocalDate());
-				usuario.setCpf(rs.getString("cpf"));
-				usuario.setEmail(rs.getString("email"));
-				usuario.setSenha(rs.getString("senha"));
-			}
-			rs.close();
-			stm.close();
-			con.close();
-			return usuario;
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
+            rs.close();
+            stm.close();
+            con.close();
+            return usuario;
 
-	public static void deleteById(int id) {
-		try {
-			Connection con = ConexaoDB.getConexao();
-			String sql = "delete from tb_usuario where id=?";
-			PreparedStatement stm = con.prepareStatement(sql);
-			stm.setInt(1, id);
-			stm.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
-			stm.close();
-			con.close();
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
+    public static void insert(Usuario usuario) {
+        try {
+            Connection con = ConexaoDB.getConexao();
+            String sql = "INSERT INTO tb_usuario (nome, email, senha, foto) VALUES (?, ?, ?, ?)";
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, usuario.getNome());
+            stm.setString(2, usuario.getEmail());
+            stm.setString(3, usuario.getSenha());
+            stm.setString(4, usuario.getFoto());
+            stm.execute();
 
-	
-	// usuarioAlterado (como atributo)
-	public static void update(Long id, Usuario usuarioAlterado) {
-		try {
-			Connection con = ConexaoDB.getConexao();
-			String sql = "update tb_usuario set nome = ?, data_nascimento = ?, cpf = ?, email = ?, senha = ? where id = ?";
-			PreparedStatement stm = con.prepareStatement(sql);
-			stm.setString(1, usuarioAlterado.getNome());
-			stm.setDate(2, java.sql.Date.valueOf(usuarioAlterado.getData_nascimento()));
-			stm.setString(3, usuarioAlterado.getCpf());
-			stm.setString(4, usuarioAlterado.getEmail());
-			stm.setString(5, usuarioAlterado.getSenha());
-			stm.setLong(7, id);
-			stm.execute();
+            stm.close();
+            con.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
-			stm.close();
-			con.close();
+    public static Usuario getUsuarioById(int id) {
+        Usuario usuario = null;
+        try {
+            Connection con = ConexaoDB.getConexao();
+            String sql = "SELECT * FROM tb_usuario WHERE id = ?";
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt(1, id);
+            ResultSet rs = stm.executeQuery();
 
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
+            if (rs.next()) {
+                usuario = new Usuario();
+                usuario.setId(rs.getLong("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));
+                usuario.setFoto(rs.getString("foto"));
+            }
+
+            rs.close();
+            stm.close();
+            con.close();
+            return usuario;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static void deleteById(int id) {
+        try {
+            Connection con = ConexaoDB.getConexao();
+            String sql = "DELETE FROM tb_usuario WHERE id = ?";
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt(1, id);
+            stm.execute();
+
+            stm.close();
+            con.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static void update(Long id, Usuario usuarioAlterado) {
+        try {
+            Connection con = ConexaoDB.getConexao();
+            String sql = "UPDATE tb_usuario SET nome = ?, email = ?, senha = ?, foto = ? WHERE id = ?";
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, usuarioAlterado.getNome());
+            stm.setString(2, usuarioAlterado.getEmail());
+            stm.setString(3, usuarioAlterado.getSenha());
+            stm.setString(4, usuarioAlterado.getFoto());
+            stm.setLong(5, id);
+            stm.execute();
+
+            stm.close();
+            con.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 }
