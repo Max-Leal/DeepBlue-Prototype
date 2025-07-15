aa2b4c17da0afab2e7a25672f5f2d12badc989a9
Max-leal <max13leal@gmail.com>
2025-07-15 13:55:38 -0300

    Melhorando o backend de agência

--- src/main/java/daos/AgenciaDao.java
+++ src/main/java/daos/AgenciaDao.java
@@ -28,8 +28,14 @@ public class AgenciaDao {
                 agencia.setEmail(rs.getString("email"));
                 agencia.setSenha(rs.getString("senha"));
                 agencia.setSituacao(Situacao.valueOf(rs.getString("situacao").toUpperCase()));
+                agencia.setDescricao(rs.getString("descricao"));
+                agencia.setCep(rs.getString("cep"));
+                agencia.setTelefone(rs.getString("telefone"));
+                agencia.setWhatsapp(rs.getString("whatsapp"));
+                agencia.setInstagram(rs.getString("instagram"));
                 agencias.add(agencia);
             }
+
             rs.close();
             stm.close();
             con.close();
@@ -56,7 +62,46 @@ public class AgenciaDao {
                 agencia.setEmail(rs.getString("email"));
                 agencia.setSenha(rs.getString("senha"));
                 agencia.setSituacao(Situacao.valueOf(rs.getString("situacao").toUpperCase()));
+                agencia.setDescricao(rs.getString("descricao"));
+                agencia.setCep(rs.getString("cep"));
+                agencia.setTelefone(rs.getString("telefone"));
+                agencia.setWhatsapp(rs.getString("whatsapp"));
+                agencia.setInstagram(rs.getString("instagram"));
             }
+
+            rs.close();
+            stm.close();
+            con.close();
+            return agencia;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static Agencia getAgenciaById(int id) {
+        Agencia agencia = null;
+        try {
+            Connection con = ConexaoDB.getConexao();
+            String sql = "SELECT * FROM tb_agencia WHERE id = ?";
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setInt(1, id);
+            ResultSet rs = stm.executeQuery();
+
+            if (rs.next()) {
+                agencia = new Agencia();
+                agencia.setId(rs.getLong("id"));
+                agencia.setNomeEmpresarial(rs.getString("nome_empresarial"));
+                agencia.setCnpj(rs.getString("cnpj"));
+                agencia.setEmail(rs.getString("email"));
+                agencia.setSenha(rs.getString("senha"));
+                agencia.setSituacao(Situacao.valueOf(rs.getString("situacao").toUpperCase()));
+                agencia.setDescricao(rs.getString("descricao"));
+                agencia.setCep(rs.getString("cep"));
+                agencia.setTelefone(rs.getString("telefone"));
+                agencia.setWhatsapp(rs.getString("whatsapp"));
+                agencia.setInstagram(rs.getString("instagram"));
+            }
+
             rs.close();
             stm.close();
             con.close();
@@ -69,13 +114,18 @@ public class AgenciaDao {
     public static void insert(Agencia agencia) {
         try {
             Connection con = ConexaoDB.getConexao();
-            String sql = "INSERT INTO tb_agencia (nome_empresarial, cnpj, email, senha, situacao) VALUES (?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO tb_agencia (nome_empresarial, cnpj, email, senha, situacao, descricao, cep, telefone, whatsapp, instagram) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement stm = con.prepareStatement(sql);
             stm.setString(1, agencia.getNomeEmpresarial());
             stm.setString(2, agencia.getCnpj());
             stm.setString(3, agencia.getEmail());
             stm.setString(4, agencia.getSenha());
             stm.setString(5, agencia.getSituacao().toString().toLowerCase());
+            stm.setString(6, agencia.getDescricao());
+            stm.setString(7, agencia.getCep());
+            stm.setString(8, agencia.getTelefone());
+            stm.setString(9, agencia.getWhatsapp());
+            stm.setString(10, agencia.getInstagram());
             stm.execute();
 
             stm.close();
@@ -85,27 +135,26 @@ public class AgenciaDao {
         }
     }
 
-    public static Agencia getAgenciaById(int id) {
+    public static void update(Long id, Agencia agenciaAlterada) {
         try {
             Connection con = ConexaoDB.getConexao();
-            String sql = "SELECT * FROM tb_agencia WHERE id = ?";
+            String sql = "UPDATE tb_agencia SET nome_empresarial = ?, cnpj = ?, email = ?, senha = ?, situacao = ?, descricao = ?, cep = ?, telefone = ?, whatsapp = ?, instagram = ? WHERE id = ?";
             PreparedStatement stm = con.prepareStatement(sql);
-            stm.setInt(1, id);
-            ResultSet rs = stm.executeQuery();
-            Agencia agencia = null;
-            if (rs.next()) {
-                agencia = new Agencia();
-                agencia.setId(rs.getLong("id"));
-                agencia.setNomeEmpresarial(rs.getString("nome_empresarial"));
-                agencia.setCnpj(rs.getString("cnpj"));
-                agencia.setEmail(rs.getString("email"));
-                agencia.setSenha(rs.getString("senha"));
-                agencia.setSituacao(Situacao.valueOf(rs.getString("situacao").toUpperCase()));
-            }
-            rs.close();
+            stm.setString(1, agenciaAlterada.getNomeEmpresarial());
+            stm.setString(2, agenciaAlterada.getCnpj());
+            stm.setString(3, agenciaAlterada.getEmail());
+            stm.setString(4, agenciaAlterada.getSenha());
+            stm.setString(5, agenciaAlterada.getSituacao().toString().toLowerCase());
+            stm.setString(6, agenciaAlterada.getDescricao());
+            stm.setString(7, agenciaAlterada.getCep());
+            stm.setString(8, agenciaAlterada.getTelefone());
+            stm.setString(9, agenciaAlterada.getWhatsapp());
+            stm.setString(10, agenciaAlterada.getInstagram());
+            stm.setLong(11, id);
+            stm.execute();
+
             stm.close();
             con.close();
-            return agencia;
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
@@ -121,27 +170,6 @@ public class AgenciaDao {
 
             stm.close();
             con.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
-
-    public static void update(Long id, 	Agencia agenciaAlterada) {
-        try {
-            Connection con = ConexaoDB.getConexao();
-            String sql = "UPDATE tb_agencia SET nome_empresarial = ?, cnpj = ?, email = ?, senha = ?, situacao = ? WHERE id = ?";
-            PreparedStatement stm = con.prepareStatement(sql);
-            stm.setString(1, agenciaAlterada.getNomeEmpresarial());
-            stm.setString(2, agenciaAlterada.getCnpj());
-            stm.setString(3, agenciaAlterada.getEmail());
-            stm.setString(4, agenciaAlterada.getSenha());
-            stm.setString(5, agenciaAlterada.getSituacao().toString().toLowerCase());
-            stm.setLong(6,id);
-            stm.execute();
-
-            stm.close();
-            con.close();
-
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
