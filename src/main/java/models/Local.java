48734eebc368c04e4c20b15983d0b752f56b6bfd
Nicolas Silva <nicolas@email.com>
2025-07-15 15:56:06 -0300

    Atualizado o backend de local

--- src/main/java/models/Local.java
+++ src/main/java/models/Local.java
@@ -6,10 +6,13 @@ public class Local {
 
 	
 	private Long id;
-	private String localidade;
-	private Situacao situacao;
 	private String nome;
+	private String localidade;
 	private String descricao;
+	private String tipo_embarcacao;
+	private int ano_afundamento;
+	private double profundidade;
+	private Situacao situacao;
 	private String longitude;
 	private String latitude;
 	
@@ -17,25 +20,17 @@ public class Local {
 		
 	}
 
-	public Local(String localidade, Situacao situacao, String nome, String descricao, String longitude,
-			String latitude) {
-		super();
-		this.localidade = localidade;
-		this.situacao = situacao;
-		this.nome = nome;
-		this.descricao = descricao;
-		this.longitude = longitude;
-		this.latitude = latitude;
-	}
-
-	public Local(Long id, String localidade, Situacao situacao, String nome, String descricao, String longitude,
-			String latitude) {
+	public Local(Long id, String nome, String localidade, String descricao, String tipo_embarcacao, int ano_afundamento,
+			double profundidade, Situacao situacao, String longitude, String latitude) {
 		super();
 		this.id = id;
-		this.localidade = localidade;
-		this.situacao = situacao;
 		this.nome = nome;
+		this.localidade = localidade;
 		this.descricao = descricao;
+		this.tipo_embarcacao = tipo_embarcacao;
+		this.ano_afundamento = ano_afundamento;
+		this.profundidade = profundidade;
+		this.situacao = situacao;
 		this.longitude = longitude;
 		this.latitude = latitude;
 	}
@@ -48,22 +43,6 @@ public class Local {
 		this.id = id;
 	}
 
-	public String getLocalidade() {
-		return localidade;
-	}
-
-	public void setLocalidade(String localidade) {
-		this.localidade = localidade;
-	}
-
-	public Situacao getSituacao() {
-		return situacao;
-	}
-
-	public void setSituacao(Situacao situacao) {
-		this.situacao = situacao;
-	}
-
 	public String getNome() {
 		return nome;
 	}
@@ -72,12 +51,52 @@ public class Local {
 		this.nome = nome;
 	}
 
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
 	public String getDescricao() {
 		return descricao;
 	}
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public String getTipo_embarcacao() {
+		return tipo_embarcacao;
+	}
+
+	public void setTipo_embarcacao(String tipo_embarcacao) {
+		this.tipo_embarcacao = tipo_embarcacao;
+	}
+
+	public int getAno_afundamento() {
+		return ano_afundamento;
+	}
+
+	public void setAno_afundamento(int ano_afundamento) {
+		this.ano_afundamento = ano_afundamento;
+	}
+
+	public double getProfundidade() {
+		return profundidade;
+	}
+
+	public void setProfundidade(double profundidade) {
+		this.profundidade = profundidade;
+	}
+
+	public Situacao getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(Situacao situacao) {
+		this.situacao = situacao;
 	}
 
 	public String getLongitude() {
@@ -95,6 +114,7 @@ public class Local {
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
-	
-	
 }
+
+	
+	
