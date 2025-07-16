edc8c100e9c7fae96076cd2bb52e468a78243545
Max-leal <max13leal@gmail.com>
2025-07-16 15:09:00 -0300

    Implementação da foto em avaliações de Local

--- src/main/java/daos/AvaliacaoLocalDao.java
+++ src/main/java/daos/AvaliacaoLocalDao.java
@@ -3,7 +3,6 @@ package daos;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
