f26b4d2ae6960533c10459b7c233de3f71424459
Max-Leal <max13leal@gmail.com>
2025-09-08 07:34:02 -0300

    Melhorando o arquivo sql

--- deepblue.sql
+++ deepblue.sql
@@ -97,3 +97,23 @@ CREATE TABLE IF NOT EXISTS tb_avaliacao_foto_local (
   PRIMARY KEY (id),
   FOREIGN KEY (id_avaliacao) REFERENCES tb_avaliacao_local(id) ON DELETE CASCADE
 ) ENGINE=InnoDB; 
+
+INSERT INTO tb_local (
+  localidade, situacao, nome, descricao, tipo_embarcacao, ano_afundamento, profundidade, latitude, longitude
+) VALUES
+('Santa Catarina', 'disponivel', 'San Miguel', 'Nau espanhola', 'Nau', NULL, NULL, '-27.58565', '-48.57048'),
+('Santa Catarina', 'disponivel', 'Guilhermina', 'Lanchão de nacionalidade desconhecida', 'Lanchão', NULL, NULL, '-27.49565', '-48.53795'),
+('Santa Catarina', 'disponivel', 'Sardinha', 'Iate brasileiro', 'Iate', NULL, NULL, '-27.46432', '-48.55442'),
+('Santa Catarina', 'disponivel', 'Porto Belgrano', 'Draga argentina', 'Draga', NULL, NULL, '-27.39640', '-48.46408'),
+('Santa Catarina', 'disponivel', 'Nome desconhecido', 'Embarcação desconhecida espanhola', 'Desconhecido', NULL, NULL, '-27.43667', '-48.37638'),
+('Santa Catarina', 'disponivel', 'Melo II', 'Iate brasileiro', 'Iate', NULL, NULL, '-27.83275', '-48.49828'),
+('Santa Catarina', 'disponivel', 'Camponês', 'Iate brasileiro', 'Iate', NULL, NULL, '-27.86057', '-48.57105'),
+('Santa Catarina', 'disponivel', 'Marie Charlotte', 'Lúgar francês', 'Lúgar', NULL, NULL, '-27.84892', '-48.57478'),
+('Santa Catarina', 'disponivel', 'Nuestra Señora de La Concépcion', 'Nau espanhola', 'Nau', NULL, NULL, '-27.84093', '-48.57023'),
+('Santa Catarina', 'disponivel', 'Nome desconhecido', 'Caravela espanhola', 'Caravela', NULL, NULL, '-27.83593', '-48.56422'),
+('Santa Catarina', 'disponivel', 'Provedora San Estebán', 'Nau espanhola', 'Nau', NULL, NULL, '-27.82840', '-48.57245'),
+('Santa Catarina', 'disponivel', 'Febo', 'Barca italiana', 'Barca', NULL, NULL, '-27.22718', '-48.42557');
+
+INSERT INTO tb_agencia (id, nome_empresarial, cnpj, email, senha, situacao, descricao, cep, telefone, whatsapp, instagram) VALUES
+(1, 'SeaDive', '12.345.678/0001-99', 'seadive@gmail.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'disponivel', 'Especialistas em mergulho e aventuras marítimas. Oferecemos certificações PADI, batismos e expedições guiadas para os mais belos naufrágios e recifes da costa. Nossa missão é conectar você ao oceano com segurança e paixão.', '88385000', '47991234567', '47991234567', 'https://www.instagram.com/seadive.aventuras'),
+(2, 'Mundo Aventuras', '98.765.432/0001-11', 'mundoaventuras@gmail.com', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'disponivel', 'Sua porta de entrada para a adrenalina e a natureza. Explore trilhas, faça rapel em cachoeiras, navegue de caiaque e mergulhe em locais incríveis. A Mundo Aventuras transforma seu desejo de explorar em realidade.', '88000000', '48988123456', '48988123456', 'https://www.instagram.com/mundo.aventuras.eco');
